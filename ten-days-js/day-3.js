da64d7c470b7a03b10bc823186cce39539bfe764
Phil Jolley <phillip.jordan.31@gmail.com>
2019-12-01 21:16:10 -0700

    Day 3 - Part 2 Try, Catch and Finally

--- ten-days-js/day-3.js
+++ ten-days-js/day-3.js
@@ -27,3 +27,28 @@ function getSecondLargest(nums) {
 //test area [2,3,6,6,5]
 const testArr = [2,3,6,6,5];
 dayThree.innerHTML = 'The Original Array: ' + testArr + ' The Second Lowest Number in the Array: ' + getSecondLargest(testArr);
+
+//day 3-part 2 (try, catch, and finally)
+let dayThreePart2 = document.querySelector('.day-3-part-2');
+//one parameter s (string)
+//1. Try to reverse a string using the split, reverse, and join methods
+//2. If an exception is thrown, catch it and print the contents of the
+// exception's message on a new line.
+//3. Print s on a new line. If no exception was thrown, then this should
+// be the reversed string; if an exception was thrown, this should be the original string.
+
+function reverseString(s) {
+    //code block
+    try {
+        console.log(s.split('').reverse().join(''));
+    } catch (e) {
+        console.log(e.message);
+        console.log(s);
+    }
+}
+let testStr = '123';
+console.log(reverseString(testStr));
+
+dayThreePart2.innerHTML = '';
+//to solve this I first made sure I could reverse the string.
+//Second, I made test cases of what it would look like if I did have an argument that wasn't a string.
