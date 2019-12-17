44a63af484c01f4346f076b1da80aa22a4122c65
Phil Jolley <phillip.jordan.31@gmail.com>
2019-12-16 20:49:03 -0700

    Day 6 Part 2 Done

--- ten-days-js/day-6.js
+++ ten-days-js/day-6.js
@@ -13,31 +13,19 @@ let daySix = document.querySelector('.day-6');
 //if you do 1 & 2 it will return 0
 
 const s = [1,2,3,4];
-const k = 4;
-// let newArr = Array();
-//
-// for(let i = 0; i < s.length; i++){
-//     for(let j = 1; j < s.length - 1; j++){
-//         //newArr.push();
-//         if(((s[i] & s[j]) !== 0) && (s[i] & s[j]) < k){
-//             newArr.push((s[i] & s[j]));
-//         }
-//     }
-// }
-// console.log(Math.max(...newArr));
+//const k = 4;
 
 function getMaxLessThanK(n,k){
-    let newArr = Array();
+    if(n >= 2 && k >= 2) {
+        let newArr = Array();
+        let numArr = Array.from(Array(n), (x, index) => index + 1);
 
-    if(k === 0){
-        return n;
-    }
-
-    for(let i = 0; i < n.length; i++){
-        for(let j = 1; j < n.length - 1; j++){
-            //newArr.push();
-            if(((n[i] & n[j]) !== 0) && (n[i] & n[j]) < k){
-                newArr.push((n[i] & n[j]));
+        for(let i = 0; i < numArr.length; i++){
+            for(let j = 1; j < numArr.length - 1; j++){
+                //newArr.push();
+                if(((numArr[i] & numArr[j]) !== 0) && (numArr[i] & numArr[j]) < k){
+                    newArr.push((numArr[i] & numArr[j]));
+                }
             }
         }
     }
@@ -45,11 +33,86 @@ function getMaxLessThanK(n,k){
     return Math.max(...newArr);
 }
 
-const testGetMax = [1,2,3,4];
+const testGetMax = 4;
 const testK = 4;
 
-console.log(getMaxLessThanK(testGetMax, testK));
+//console.log(getMaxLessThanK(testGetMax, testK));
+//let newArr = Array.from(Array(4), (x, index) => index + 1);
+//console.log(newArr);
 
 //I missread I thought n was an array. It's not. It's an integer. I will change it up tomorrow.
 
 daySix.innerHTML = '';
+
+//while loop test
+let testKey = 5;
+let a = 1;
+let kk = 2;
+let newArr = Array();
+while(testKey > a) {
+    console.log('a: ' + a);
+    let b = a + 1;
+    while(testKey >= b) {
+        console.log('a: ' + a + ' ' + 'b: ' + b);
+        if( (a & b) < kk ){
+            newArr.push(a & b);
+        }
+        b++;
+    }
+    a++;
+}
+console.log(Math.max(...newArr));
+/*
+* test cases
+5 2
+8 5
+2 2
+*
+* */
+function getMaxLessThanK(n,k) {
+    if(k >= 2) {
+        let a = 1;
+        let newArr = Array();
+        while(n > a) {
+            let b = a + 1;
+            while(n >= b) {
+                if( (a & b) < k ){
+                    newArr.push(a & b);
+                }
+                b++;
+            }
+            a++;
+        }
+        return Math.max(...newArr);
+    }
+}
+console.log(getMaxLessThanK(3));
+
+let daySixPart2 = document.querySelector('.day-6-part-2');
+
+//code block
+function getDayName(dateString) {
+    let dayName = new Date(dateString);
+    dayName = dayName.getDay();
+    if(dayName === 0) {
+        dayName = 'Sunday';
+    } else if(dayName === 1) {
+        dayName = 'Monday';
+    } else if(dayName === 2) {
+        dayName = 'Tuesday';
+    } else if(dayName === 3) {
+        dayName = 'Wednesday';
+    } else if(dayName === 4) {
+        dayName = 'Thursday';
+    } else if(dayName === 5) {
+        dayName = 'Friday';
+    } else if(dayName === 6) {
+        dayName = 'Saturday';
+    }
+    return dayName;
+}
+//let findDate = new Date('10/11/2009');
+console.log('this date is: ' + getDayName('10/11/2009'));
+//Date.getDay()
+
+daySixPart2.innerHTML = '';
